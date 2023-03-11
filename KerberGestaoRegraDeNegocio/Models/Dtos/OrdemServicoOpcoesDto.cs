05ef6f3f829a29b58d9ac5f0075a36d2b2a0cada
Joao Marcos Carniel <joao.carniel@hub2b.com.br>
2023-03-10 23:35:38 -0300

    Tentativas do multiselect comentadas

--- KerberGestaoRegraDeNegocio/Models/Dtos/OrdemServicoOpcoesDto.cs
+++ KerberGestaoRegraDeNegocio/Models/Dtos/OrdemServicoOpcoesDto.cs
@@ -6,6 +6,7 @@ namespace KerberGestaoRegraDeNegocio.Models.Dtos
     {
         public List<ClienteDto>? Clientes { get; set; }
         public List<OrcamentoDto>? Orcamentos { get; set; }
+        public List<int>? OrcamentosList { get; set; }
         public int IdCliente { get; set; }
         public int IdOrcamento { get; set; }
         public string Titulo { get; set; } = null!;
